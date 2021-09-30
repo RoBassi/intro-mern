5e72deb44741eb5e61ded291eb4b05b381c6acc1
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-30 17:37:35 -0300

    push

--- intro-mern-frontend/src/Products/components/ProductLayout.js
+++ intro-mern-frontend/src/Products/components/ProductLayout.js
@@ -1,21 +1,70 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { Modal, Container } from 'react-bulma-components'
 import Header from './Header'
 import AddButton from './AddButton'
+import ListProducts from './ListProducts'
+import Form from './Form'
 import Loading from './Loading'
+import { saveProduct, getProducts } from '../services'
 
 const ProductLayout = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
     const [isLoading, setIsLoading] = useState(true)
+    const [products, setProducts] = useState([])
+
+    async function loadProducts () {
+        const response = await getProducts()
+        
+        if (response.status === 200) {
+            setProducts(response.data.products)
+        }
+    
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        loadProducts()
+    }, [])
+
+    const handleSubmit = async (data) => {
+        await saveProduct(data)
+        loadProducts()
+        setIsModalOpen(false)
+    }
 
     return (
-        <>
+        <Container>
             <Header title="Products app" />
-            <AddButton />
+            <AddButton onClick={() => setIsModalOpen(true)} />
             {
-                isLoading
-                ? <Loading />
-                : 'mostrar resultado fetch'
+                isLoading && <Loading />           
             }
-        </>
+
+            {
+                !isLoading && !products.length && (
+                    <h2 className="title has-text-centered">
+                        You don't have products
+                    </h2>
+                )
+            }
+
+            {
+                isLoading && products.length && <ListProducts products={products} />
+            }
+
+            <Modal show={isModalOpen} onClose={() => setIsModalOpen(false)}>
+                <Modal.Card>
+                    <Modal.Card.Header showClose={false}>
+                    <Modal.Card.Title>
+                        Add Product
+                    </Modal.Card.Title>
+                    </Modal.Card.Header>
+                    <Modal.Card.Body>
+                        <Form handleSubmit={handleSubmit} />
+                    </Modal.Card.Body>
+                </Modal.Card>
+            </Modal>
+        </Container>
     )
 }
 
